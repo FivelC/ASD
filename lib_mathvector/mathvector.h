42659a3d8f7fa15bc4ad75a5c870ed66c8775e3f
FivelC <elapin2006@gmail.com>
2025-09-18 17:20:37 +0300

    init MathVector

---
+++ lib_mathvector/mathvector.h
@@ -0,0 +1,17 @@
+
+#ifndef _MATHVECTOR_MATHVECTOR_H_
+#define _MATHVECTOR_MATHVECTOR_H_
+
+template <typename T>
+class MathVector
+{
+public:
+	MathVector();
+	~MathVector();
+
+private:
+
+};
+
+
+#endif // !_MATHVECTOR_MATHVECTOR_H_
