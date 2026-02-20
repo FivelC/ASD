1125e61cf51c27f405ad3c138001ed9dd974b882
FivelC <elapin2006@gmail.com>
2026-02-20 23:01:25 +0300

    start monom. think about split with Stack or tmp

---
+++ lib_monom/monom.cpp
@@ -0,0 +1,20 @@
+#include "monom.h"
+#include <cctype>
+
+void expression(std::string exem, unsigned int& x_degre, unsigned int& y_degre, unsigned int& z_degre, double& coef){
+	Stack<char> split;
+
+	
+	/*for (int i = 0; i < exem.size() - 1; i++) {
+		char tmp;
+		int queue = 0; 
+		if (isdigit(exem[i])) {
+			tmp += exem[i];
+		}
+		else if (isalpha(exem[i])) { 
+			if (queue == 0) {
+				coef = tmp;
+			}
+		}
+	}*/
+}
--- lib_monom/monom.h
+++ lib_monom/monom.h
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
+#include "../lib_stack/stack.h"
 class Monom {
-	std::string expression;
-	
+	unsigned int x_degre = 0, y_degre = 0, z_degre = 0;
+	double coef;
+public:
+	void expression(std::string, unsigned int*, unsigned int*, unsigned int*, double* coef);
+
 };
