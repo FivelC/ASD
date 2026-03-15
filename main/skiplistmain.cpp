b7c725263e5e796a8493c37ff2a597e3ef6ef0e5
FivelC <elapin2006@gmail.com>
2026-03-16 00:15:17 +0300

    bla bla

---
+++ main/skiplistmain.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include "../lib_skiplist/skiplist.h"
+
+int main() {
+    skipList<int, std::string> myMap;
+
+    std::cout << "=== Testing Insertion ===" << std::endl;
+    myMap.insert(10, "Ten");
+    myMap.insert(20, "Twenty");
+    myMap.insert(5, "Five");
+    myMap.insert(15, "Fifteen");
+    myMap.insert(25, "Twenty-Five");
+
+    myMap.insert(10, "TEN_UPDATED");
+
+    myMap.display();
+
+    std::cout << "=== Testing Search ===" << std::endl;
+    int testKeys[] = { 5, 10, 100 };
+
+    for (int key : testKeys) {
+        if (myMap.search(key)) {
+            std::cout << "Key " << key << " found in list." << std::endl;
+        }
+        else {
+            std::cout << "Key " << key << " NOT found." << std::endl;
+        }
+    }
+
+    std::cout << "\n=== Testing with different types (double, char) ===" << std::endl;
+    skipList<double, int> charMap;
+    charMap.insert(1.1, 1);
+    charMap.insert(2.2, 2);
+    charMap.insert(0.5, 0);
+    charMap.display();
+
+    return 0;
+}
